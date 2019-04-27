51c9bb3f1b1e7ff828dae8683d67dcdfaee558eb
Ivan <startsev.ia@phystech.edu>
2019-04-27 18:19:10 +0300

    Update tic tac toe.cpp

--- tic tac toe.cpp
+++ tic tac toe.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 const int X = 1;
 const int O = 2;
+const int onlyWins = 1;
+const int exactMoves = 2;
 bool thisCollocIsWinning(map<int, int>& t) {
 	for (int i = 0; i < 3; i++) {
 		if ((t[1 + 3 * i] == t[2 + 3 * i] && t[2 + 3 * i] == t[3 + 3 * i] && t[3 + 3 * i] != 0)
@@ -42,40 +44,52 @@ void printCombinations(set<map<int, int>> comb) {
 	for (auto item : comb) {
 		if (!thisCollocIsWinning(item)) {
 			count++;
-
 			PrintCombination(item);
 		}
 	}
 	cout << "amount of draws = " << count << endl;
 }
 
-void makeAMove(int currDepth, map<int, int> currCollocation, int lastStep, set<map<int, int>>& comb, int& amount) {
-	if (thisCollocIsWinning(currCollocation)) {
-		comb.insert(currCollocation);
-		amount++;
-		return;
-	}
-	if (currDepth == lastStep) {
-		for (int i = 0; i < 9; i++) {
-			if (currCollocation[i + 1] == 0) {
-				if (lastStep % 2 == 0) {
-					currCollocation[i + 1] = X;
-				}
-				else {
-					currCollocation[i + 1] = O;
-				}
-				comb.insert(currCollocation);
-				currCollocation[i + 1] = 0;
-			}
+void makeAMove(int currDepth, map<int, int> currCollocation, int lastStep, set<map<int, int>>& comb, int& amount, int flag) {
+	if (flag == exactMoves) {
+		if (thisCollocIsWinning(currCollocation)) {
+			comb.insert(currCollocation);
+			amount++;
+			return;
 		}
-		amount++;
-		return;
+		if (currDepth == (lastStep - 1)) {
+			for (int i = 0; i < 9; i++) {
+				if (currCollocation[i + 1] == 0) {
+					if ((lastStep - 1) % 2 == 0) {
+						currCollocation[i + 1] = X;
+					}
+					else {
+						currCollocation[i + 1] = O;
+					}
+					comb.insert(currCollocation);
+					currCollocation[i + 1] = 0;
+				}
+			}
+			amount++;
+			return;
+		}
 	}
+	else {
+		if (thisCollocIsWinning(currCollocation) && currDepth == lastStep) {
+			comb.insert(currCollocation);
+			amount++;
+			return;
+		}
+		else if (thisCollocIsWinning(currCollocation) && currDepth != lastStep){
+			return;
+		}
+	}
+	
 	if (currDepth % 2 == 0) {
 		for (int i = 0; i < 9; i++) {
 			if (currCollocation[i + 1] == 0) {
 				currCollocation[i + 1] = X;
-				makeAMove(currDepth + 1, currCollocation, lastStep, comb, amount);
+				makeAMove(currDepth + 1, currCollocation, lastStep, comb, amount, flag);
 				currCollocation[i + 1] = 0;
 			}
 		}
@@ -84,7 +98,7 @@ void makeAMove(int currDepth, map<int, int> currCollocation, int lastStep, set<m
 		for (int i = 0; i < 9; i++) {
 			if (currCollocation[i + 1] == 0) {
 				currCollocation[i + 1] = O;
-				makeAMove(currDepth + 1, currCollocation, lastStep, comb, amount);
+				makeAMove(currDepth + 1, currCollocation, lastStep, comb, amount, flag);
 				currCollocation[i + 1] = 0;
 			}
 		}
@@ -100,10 +114,10 @@ int main() {
 		currCollocation.insert(make_pair(i, 0));
 	}
 	int amount = 0;
-	makeAMove(0, currCollocation, 8, comb, amount);
+	makeAMove(0, currCollocation, 9, comb, amount, exactMoves); // onlyWins exactMoves
 	printCombinations(comb);
 	cout << "uniq comb = " << comb.size() << endl;
-	cout << "possible batches = " << amount << endl; 
+	cout << "possible batches = " << amount << endl;
 
 	system("pause");
 
